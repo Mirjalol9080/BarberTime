0e74a111b89fdcf59f0c4f948f2e6ce1cd7efbd9
Mirjalol9080 <pubgmabile969@gmail.com>
2024-11-30 19:06:00 +0500

    M

--- app.js
+++ app.js
@@ -10,3 +10,13 @@ marquee.addEventListener("mouseover", () => {
 marquee.addEventListener("mouseout", () => {
   marquee.start(); // Marquee qayta harakat qiladi
 });
+
+
+
+const music = document.getElementById("background-music");
+  const playButton = document.getElementById("play-music");
+
+  playButton.addEventListener("click", () => {
+    music.play();
+    playButton.style.display = "none"; // Tugmani yashirish
+  });
